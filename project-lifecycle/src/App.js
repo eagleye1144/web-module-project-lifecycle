fa4adcb62a0c334cc459aa3bd73022444323fbea
Mitchell Walker <eagleye1144@gmail.com>
2021-04-07 21:07:59 -0600

    MVP done

--- project-lifecycle/src/App.js
+++ project-lifecycle/src/App.js
@@ -4,30 +4,44 @@ import axios from 'axios';
 import UserCard from './UserCard'
 
 
+
 class App extends React.Component {
   state = {
     myGit: {},
-    followersGit: []
+    followers: {},
   }
 
   componentDidMount() {
     axios.get('https://api.github.com/users/eagleye1144')
     .then(res =>{
       console.log(res.data)
-      this.setState(res.data)
-
-
+      this.setState({
+        myGit: res.data
+      })
     })
     .catch(err =>{
 
       console.log(err, "ERROR!!!!")
     })
+    axios.get('https://api.github.com/users/eagleye1144/followers')
+      .then(res=>{
+        this.setState({
+          followers: res.data
+
+        })
+        console.log(res.data)
+
+      })
+
+
+
   }
 
   render() {
+    const {myGit, followers} = this.state
   return (
     <div className="App">
-      <UserCard/>
+      <UserCard info={myGit} followers={followers}/>
     </div>
   );
   }
--- project-lifecycle/src/UserCard.js
+++ project-lifecycle/src/UserCard.js
@@ -1,16 +1,33 @@
 import React from 'react'
-
+import styled from 'styled-components'
 
 
 export default function UserCard(props) {
-    
+    const CardDiv = styled.div`
+        background-color: #DAC6B6;
+        padding: 5%;
+        margin: auto;
+    img{
+        border-radius: 50px 50px 50px 50px;
+        margin: 2%;
+    }
+    `;
 
+    const {info, followers} = props
+    console.log(info.name)
     return(
 
-        <div>
+        <CardDiv>
             <h1>GitHub Card</h1>
-
-        </div>
+            <div className= "info">
+                <div id= 'name'>{info.name}</div>
+                <div id = 'location'>{info.location}</div>
+                <div className = 'img'>
+                    <img src = {info.avatar_url} alt ="Avatar"/>
+                </div>
+                <p>You have {followers.length} followers haha</p>
+            </div>
+        </CardDiv>
 
     )
 }
